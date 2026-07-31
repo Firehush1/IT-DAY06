3d7150528c15a6d4adf7e255ac8c9999abf5e279
Firehush1 <hansanikalhari99@gmail.com>
2026-07-31 20:56:25 +0530

    Refactor app.js to define Customer class and instantiate customer objects

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("Hai HASHU");
+class Customer {
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+}
+
+let customer1 = new Customer("HUSH", 30, "Australia");
+console.log(customer1);
+
+
+// Output: Customer { name: 'HUSH', age: 30, address: 'Australia' }
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+let customer2 = {
+        name: "HUSH",
+        age: 30,
+        address: "Australia"
+        };
+
+        console.log(customer2);
