f99a5fa75a4de7768dc0c4b955bff73722f4fffc
Firehush1 <hansanikalhari99@gmail.com>
2026-08-02 20:57:10 +0530

    Comment out Customer class and related code; add salary and items properties to customer2 object

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-class Customer {
+// class Customer {
 
-    name;
-    age;
-    address;
+//     name;
+//     age;
+//     address;
 
-    constructor(name, age, address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
+//     constructor(name, age, address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
 
-    getName() {
-        return this.name;
-    }
-    setName(name) {
-        this.name = name;
-    }
-}
+//     getName() {
+//         return this.name;
+//     }
+//     setName(name) {
+//         this.name = name;
+//     }
+// }
 
-let customer1 = new Customer("HUSH", 30, "Australia");
-console.log(customer1);
-console.log(customer1.getName());
+// let customer1 = new Customer("HUSH", 30, "Australia");
+// console.log(customer1);
+// console.log(customer1.getName());
 
 
 // Output: Customer { name: 'HUSH', age: 30, address: 'Australia' }
@@ -31,9 +31,17 @@ let customer2 = {
         name: "HUSH",
         age: 30,
         address: "Australia",
-        salary: 10000
-        };
+        salary: 10000,
+        items: ["item1", "item2", "item3"],
+        father: {
+            name: "John",
+            age: 60,
+            address: "USA"
+        }
+    };
 
         console.log(customer2.name);
         console.log(customer2.age);
+        console.log(customer2.address);
         console.log(customer2.salary);
+        console.log(customer2.items.includes("item2"));
