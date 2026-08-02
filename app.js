f7678403fd97bc9ab5dfd8c81fa55d5d84982ab1
Firehush1 <hansanikalhari99@gmail.com>
2026-08-02 20:48:10 +0530

    Add salary property to customer2 object and log its value

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 class Customer {
+
+    name;
+    age;
+    address;
+
     constructor(name, age, address) {
         this.name = name;
         this.age = age;
         this.address = address;
     }
+
+    getName() {
+        return this.name;
+    }
+    setName(name) {
+        this.name = name;
+    }
 }
 
 let customer1 = new Customer("HUSH", 30, "Australia");
 console.log(customer1);
+console.log(customer1.getName());
 
 
 // Output: Customer { name: 'HUSH', age: 30, address: 'Australia' }
@@ -17,7 +30,10 @@ console.log(customer1);
 let customer2 = {
         name: "HUSH",
         age: 30,
-        address: "Australia"
+        address: "Australia",
+        salary: 10000
         };
 
-        console.log(customer2);
+        console.log(customer2.name);
+        console.log(customer2.age);
+        console.log(customer2.salary);
